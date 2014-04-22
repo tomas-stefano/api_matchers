0a7431a4255f084224e6c40798e7353035dbfbc0
Ahmet AYGÜN <me@ahmetaygun.net>
2014-04-22 15:55:32 +0300

    Fix deprecation warnings in RSpec 3, fully backwards compatible.

--- lib/api_matchers/headers/be_json.rb
+++ lib/api_matchers/headers/be_json.rb
@@ -5,13 +5,17 @@ module APIMatchers
         'application/json; charset=utf-8'
       end
 
-      def failure_message_for_should
+      def failure_message
         %Q{expected a JSON response with '#{expected_content_type}'. Got: '#{content_type_response}'.}
       end
 
-      def failure_message_for_should_not
+      def failure_message_when_negated
         %Q{expected to not be a JSON response. Got: '#{expected_content_type}'.}
       end
+
+      # RSpec 2 compatibility:
+      alias_method :failure_message_for_should, :failure_message
+      alias_method :failure_message_for_should_not, :failure_message_when_negated
     end
   end
 end
--- lib/api_matchers/http_status_code/create_resource.rb
+++ lib/api_matchers/http_status_code/create_resource.rb
@@ -5,13 +5,17 @@ module APIMatchers
         201
       end
 
-      def failure_message_for_should
+      def failure_message
         %Q{expected that '#{@http_status_code}' to be Created Resource with the status '201'.}
       end
 
-      def failure_message_for_should_not
+      def failure_message_when_negated
         %Q{expected that '#{@http_status_code}' to NOT be Created Resource.}
       end
+
+      # RSpec 2 compatibility:
+      alias_method :failure_message_for_should, :failure_message
+      alias_method :failure_message_for_should_not, :failure_message_when_negated
     end
   end
 end
