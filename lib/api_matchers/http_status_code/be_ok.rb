244acd434a691ce337dd131e54aa9fd078019f95
Ralf Schmitz Bongiolo <ralfsb2008@gmail.com>
2014-11-28 01:27:02 -0200

    testing matcher with description.

--- lib/api_matchers/http_status_code/be_ok.rb
+++ lib/api_matchers/http_status_code/be_ok.rb
@@ -13,6 +13,10 @@ module APIMatchers
         %Q{expected that '#{@http_status_code}' to NOT be ok with the status '200'.}
       end
 
+      def description
+        "be ok"
+      end
+
       # RSpec 2 compatibility:
       alias_method :failure_message_for_should, :failure_message
       alias_method :failure_message_for_should_not, :failure_message_when_negated
